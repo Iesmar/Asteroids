3e10186ca83e72ae4e1a226b1ca1b1fb4712efa7
Iesmar <vincent.peschar@gmail.com>
2026-04-28 14:07:48 +0200

    CH3 L3: moving around learning about rotation.

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
-        
+       
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         
         screen.fill(("black"))
+        player.draw(screen)
+
+        player.update(dt)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # Calculate delta time in seconds
+        # print(f"Delta time: {dt} seconds")
     
 
 if __name__ == "__main__":
